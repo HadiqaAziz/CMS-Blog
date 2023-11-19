c3a06d1b91e04db5752af6f9bf17ccb1eda738d8
Hadiqa <hadiqa.aziz@gmail.com>
2023-11-19 07:36:44 -0800

    Update helpers.js

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,4 +1,5 @@
 module.exports = {
+    // the format in which date will be shown on screen
     format_date: date => {
         return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(date).getFullYear()}`;
     }
