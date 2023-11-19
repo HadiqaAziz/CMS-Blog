64832342a3866fb81628334224f02a4efd8b6c8a
Hadiqa <hadiqa.aziz@gmail.com>
2023-11-19 07:42:16 -0800

    including response

--- public/javascript/delete-post.js
+++ public/javascript/delete-post.js
@@ -1,6 +1,6 @@
 async function deleteFormHandler(event) {
     event.preventDefault();
-
+    // split on the screen where the post needs to deleted
     const id = window.location.toString().split('/')[
         window.location.toString().split('/').length - 1
       ];
